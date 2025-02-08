d732b6a6412b33f3a2994244a2b0fb0c256841a4
Tamas Kurics <tamas.kurics@gmail.com>
2025-02-08 14:24:22 +0100

    day 4
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ src/day04a.ml
@@ -0,0 +1,13 @@
+let find_seed (key : string) : int =
+  let rec loop n =
+    let s = key ^ (string_of_int n) in
+    let digest = Digest.MD5.string s in
+    if String.starts_with ~prefix:"00000" (Digest.MD5.to_hex digest) then n
+    else loop (n + 1)
+  in loop 1
+
+
+let () =
+  let secret_key = "bgvyzdsv" in
+  let result = find_seed secret_key in
+  print_int result; print_newline ();

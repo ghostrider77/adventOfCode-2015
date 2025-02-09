440270e078032fbe5ac332ba9bb05d04e275f36b
Tamas Kurics <tamas.kurics@gmail.com>
2025-02-09 11:54:48 +0100

    day 6
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ src/day06a.ml
@@ -0,0 +1,44 @@
+type coord = {x : int; y : int}
+type action = TurnOn | TurnOff | Toggle
+type instruction = {switch : action; topLeft : coord; bottomRight : coord}
+
+let action_of_string = function
+  | "on" -> TurnOn
+  | "off" -> TurnOff
+  | _ -> failwith "Unknown action."
+
+
+let parse_input (lines : string list) : instruction list =
+  let parse line =
+    if String.starts_with ~prefix:"toggle" line then
+      let (a, b, c, d) = Scanf.sscanf line "toggle %d,%d through %d,%d" (fun a b c d -> (a, b, c, d)) in
+      {switch = Toggle; topLeft = {x = a; y = b}; bottomRight = {x = c; y = d}}
+    else
+      let (switch, a, b, c, d) = Scanf.sscanf line "turn %s %d,%d through %d,%d" (fun s a b c d -> (s, a, b, c, d)) in
+      {switch = action_of_string switch; topLeft = {x = a; y = b}; bottomRight = {x = c; y = d}} in
+  List.map parse lines
+
+
+let adjust_lights (grid : bool array array) ({switch; topLeft; bottomRight} : instruction) : unit =
+  let light_adjuster = match switch with
+    | Toggle -> not
+    | TurnOn -> (||) true
+    | TurnOff -> (&&) false in
+  for i = topLeft.x to bottomRight.x do
+    for j = topLeft.y to bottomRight.y do
+      grid.(i).(j) <- light_adjuster grid.(i).(j);
+    done;
+  done
+
+
+let calc_nr_lits_on (instructions : instruction list) : int =
+  let grid = Array.make_matrix 1000 1000 false in
+  List.iter (adjust_lights grid) instructions;
+  Array.fold_left (fun acc row -> Array.fold_left (fun count on -> if on then count + 1 else count) acc row) 0 grid
+
+
+let () =
+  let lines = In_channel.input_lines stdin in
+  let instructions = parse_input lines in
+  let result = calc_nr_lits_on instructions in
+  print_int result; print_newline ();

e728af0d5dc17bf8a242e35c9299b8fcdd1cdfa8
Tamas Kurics <tamas.kurics@gmail.com>
2025-02-20 18:41:00 +0100

    day 18
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ src/day18a.ml
@@ -0,0 +1,47 @@
+type coord = {x : int; y : int}
+
+module CoordSet = Set.Make(
+  struct
+    type t = coord
+    let compare = Stdlib.compare
+  end)
+
+type configuration = {lights : CoordSet.t; nr_rows : int; nr_cols : int}
+
+
+let parse_input = function
+  | [] -> failwith "Empty input."
+  | (row :: _) as rows ->
+      let nr_rows = List.length rows in
+      let nr_cols = String.length row in
+      let lights = rows
+        |> List.to_seq
+        |> Seq.mapi (fun x r -> Seq.filter_map (fun (y, c) -> if c = '#' then Some {x; y} else None) (String.to_seqi r))
+        |> Seq.concat
+        |> CoordSet.of_seq in
+      {lights; nr_rows; nr_cols}
+
+
+let run_animation ({nr_rows; nr_cols; _} as initial_state : configuration) (nr_steps : int) : int =
+  let get_neighbors {x; y} = [
+      {x = x - 1; y}; {x = x - 1; y = y + 1}; {x; y = y + 1}; {x = x + 1; y = y + 1};
+      {x = x + 1; y}; {x = x + 1; y = y - 1}; {x; y = y - 1}; {x = x - 1; y = y - 1}
+    ] in
+  let coords = List.concat_map (fun x -> List.init nr_cols (fun y -> {x; y})) @@ List.init nr_rows Fun.id in
+  let rec loop ({lights; _} as state) k =
+    if k = nr_steps then CoordSet.cardinal lights
+    else
+      let is_light_on c =
+        let neighbors_on = c |> get_neighbors |> List.filter (fun n -> CoordSet.mem n lights) |> List.length in
+        if CoordSet.mem c lights then (neighbors_on = 2 || neighbors_on = 3)
+        else neighbors_on = 3 in
+      loop {state with lights = coords |> List.filter is_light_on |> CoordSet.of_list} (k + 1) in
+  loop initial_state 0
+
+
+let () =
+  let lines = In_channel.input_lines stdin in
+  let grid = parse_input lines in
+  let nr_steps = 100 in
+  let result = run_animation grid nr_steps in
+  print_int result; print_newline ();
